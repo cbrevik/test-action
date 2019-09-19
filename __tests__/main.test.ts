5ca41ffa2b380e64dbc93df0212dfc29956a45e1
Christian Brevik <christian.brevik@gmail.com>
2019-09-19 10:56:08 +0200

    Change to welcome message action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,23 @@
-describe('TODO - Add a test suite', () => {
-  it('TODO - Add a test', async () => {});
+const nock = require('nock');
+const path = require('path');
+
+describe('action test suite', () => {
+  it('It posts a comment on an opened issue', async () => {
+    const welcomeMessage = 'hello';
+    const repoToken = 'token';
+
+    process.env['INPUT_WELCOME-MESSAGE'] = welcomeMessage;
+    process.env['INPUT_REPO-TOKEN'] = repoToken;
+    process.env['GITHUB_REPOSITORY'] = 'foo/bar';
+    process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json');
+
+    nock('https://api.github.com')
+      .persist()
+      .post('/repos/foo/bar/issues/10/comments', '{\"body\":\"hello\"}')
+      .reply(200);
+    
+    const main = require('../src/main');
+
+    await main.run();
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-async function run() {
+export async function run() {
   try {
-    const name = core.getInput('name');
-    console.log(`Hello ${name}`);
+    if (github.context.payload.action !== 'opened') {
+      console.log('No issue or pull request was opened, skipping');
+      return;
+    }
+
+    const welcomeMessage: string = core.getInput('welcome-message', {required: true});
+    const repoToken: string = core.getInput('repo-token', {required: true});
+    const issue: {owner: string; repo: string; number: number} = github.context.issue;
+
+    const client: github.GitHub = new github.GitHub(repoToken);
+    await client.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: welcomeMessage
+    });
   } catch (error) {
     core.setFailed(error.message);
+    throw error;
   }
 }
 
