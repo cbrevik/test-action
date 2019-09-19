6929ea1da629e1e4f6a70a79bd2b997ab2412275
Christian Brevik <christian.brevik@gmail.com>
2019-09-19 10:35:46 +0200

    Actually console.log

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const name = core.getInput('name');
-    core.debug(`Hello ${name}`);
+    console.log(`Hello ${name}`);
   } catch (error) {
     core.setFailed(error.message);
   }
