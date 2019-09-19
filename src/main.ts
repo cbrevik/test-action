d6e070f5be6658ca38b9d55111d86a4f202d49f4
Christian Brevik <christian.brevik@gmail.com>
2019-09-19 09:45:33 +0200

    Hello world

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const name = core.getInput('name');
+    core.debug(`Hello ${name}`);
   } catch (error) {
     core.setFailed(error.message);
   }
