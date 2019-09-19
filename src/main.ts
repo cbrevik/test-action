26b6ced139ccfa33f2206ee0082d01edb0472b57
Christian Brevik <christian.brevik@gmail.com>
2019-09-19 11:31:06 +0200

    Get PR files and list them in comment

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,46 @@ import * as github from '@actions/github';
 
 export async function run() {
   try {
-    if (github.context.payload.action !== 'opened') {
-      console.log('No issue or pull request was opened, skipping');
-      return;
-    }
-
-    const welcomeMessage: string = core.getInput('welcome-message', {required: true});
+    
+    const { pull_request, action } = github.context.payload;
+    
+    if (!pull_request 
+      || (action !== 'opened' && action !== 'edited')) {
+        console.log('No pull request was opened or edited, skipping');
+        return;
+      }
+      
     const repoToken: string = core.getInput('repo-token', {required: true});
-    const issue: {owner: string; repo: string; number: number} = github.context.issue;
 
     const client: github.GitHub = new github.GitHub(repoToken);
+
+    const { owner, repo } = github.context.repo;
+
+    const prFilesResponse = await client.pulls.listFiles({
+      owner: owner,
+      repo: repo,
+      pull_number: pull_request.number
+    });
+
     await client.issues.createComment({
-      owner: issue.owner,
-      repo: issue.repo,
-      issue_number: issue.number,
-      body: welcomeMessage
+      owner: owner,
+      repo: repo,
+      issue_number: pull_request.number,
+      body: `These are the changed files: ${prFilesResponse.data.map((file) => file.filename).join(", ")}`
     });
   } catch (error) {
     core.setFailed(error.message);
     throw error;
   }
+}
+
+function getPrNumber(): number | undefined {
+  const pullRequest = github.context.payload.pull_request;
+  if (!pullRequest) {
+    return undefined;
+  }
+
+  return pullRequest.number;
 }
 
 run();
