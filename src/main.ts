f2aaa9868b841c210a0ce408f4d82e676a411205
Christian Brevik <christian.brevik@gmail.com>
2019-09-20 23:36:13 +0200

    Add check run handling

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,70 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import prettier from "prettier"
+import { WebhookPayload } from '@actions/github/lib/interfaces';
+
+export async function handlePullRequest(payload: WebhookPayload)
+{
+  const { pull_request } = payload;
+
+  if (!pull_request) {
+    throw new Error("pull_request was not defined");
+  }
+
+  const { ref: headRef, sha: headSha }  = pull_request.head;
+
+  const repoToken: string = core.getInput('repo-token', {required: true});
+
+  const client: github.GitHub = new github.GitHub(repoToken);
+
+  const { owner, repo } = github.context.repo;
+
+  const prFilesResponse = await client.pulls.listFiles({
+    owner,
+    repo,
+    pull_number: pull_request.number
+  });
+
+  var fileShas = prFilesResponse.data.map(f => ({ sha: f.sha, filename: f.filename }));
+
+  const fileContents = await Promise.all(fileShas.map(({ filename }) => getContent(client, owner, repo, filename, headRef)));
+
+  const formattedFiles = await Promise.all(fileContents.map(async ({ filename, content }) => {
+    const fileInfo = await prettier.getFileInfo(filename);
+    return {
+      shouldFormat: prettier.check(content, { parser: fileInfo.inferredParser }),
+      filename
+    }
+  }));
+
+  await client.checks.create({
+    owner,
+    repo,
+    name: "Check if needs to be formatted",
+    head_sha: headSha,
+    conclusion: "action_required",
+    actions: [
+      { label: "Fix formatting", description: "Fix formatting of file", identifier: "FORMATY_FOX" }
+    ]
+  })
+}
+
+export async function handleRequestedAction(payload: WebhookPayload) {
+  console.log(payload.check_run);
+}
 
 export async function run() {
   try {
     
-    const { pull_request, action } = github.context.payload;
+    const { eventName, action } = github.context.payload;
     
-    if (!pull_request 
-      || (action !== 'opened' && action !== 'edited')) {
-        console.log('No pull request was opened or edited, skipping');
-        return;
-      }
-
-    const { ref: headRef, sha: headSha }  = pull_request.head;
-
-    const repoToken: string = core.getInput('repo-token', {required: true});
-
-    const client: github.GitHub = new github.GitHub(repoToken);
-
-    const { owner, repo } = github.context.repo;
-
-    const prFilesResponse = await client.pulls.listFiles({
-      owner,
-      repo,
-      pull_number: pull_request.number
-    });
-
-    var fileShas = prFilesResponse.data.map(f => ({ sha: f.sha, filename: f.filename }));
-
-    const fileContents = await Promise.all(fileShas.map(({ filename }) => getContent(client, owner, repo, filename, headRef)));
-
-    const formattedFiles = await Promise.all(fileContents.map(async ({ filename, content }) => {
-      const fileInfo = await prettier.getFileInfo(filename);
-      return {
-        shouldFormat: prettier.check(content, { parser: fileInfo.inferredParser }),
-        filename
-      }
-    }));
-
-    await client.checks.create({
-      owner,
-      repo,
-      name: "Check if needs to be formatted",
-      head_sha: headSha,
-      conclusion: "action_required",
-      actions: [
-        { label: "Fix formatting", description: "Fix formatting of file", identifier: "FORMATY_FOX" }
-      ]
-    })
+    if (eventName ===  "pull_request"
+      && (action === 'opened' || action === 'edited' || action === 'synchronize')) {
+      handlePullRequest(github.context.payload)
+    }
+    if (eventName === "check_run" && action === "requested_action") {
+      handleRequestedAction(github.context.payload)
+    }
   } catch (error) {
     core.setFailed(error.message);
     throw error;
